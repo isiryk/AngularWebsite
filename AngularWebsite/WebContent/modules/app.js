7bc52a313a78218c853beeddd3ed47da83945e71
Ivan Siryk <siryk.ivan@gmail.com>
2019-01-29 20:17:12 -0500

    Learned ng-show/hide, creating a controller, $scope, and filters

--- AngularWebsite/WebContent/modules/app.js
+++ AngularWebsite/WebContent/modules/app.js
@@ -2,4 +2,37 @@ var app = angular.module('myApp', []);
 
 app.controller('MyController', ['$scope', function($scope){
 	
+	$scope.fname = 'Ivan';
+	$scope.lname = 'S';
+	
+	$scope.fullname = function(){
+		return 'My name is ' + $scope.fname + ' ' + $scope.lname;
+	}
+	
+	$scope.isSpy = true;
+	$scope.toggleSpy = function(){
+			$scope.isSpy = !$scope.isSpy
+	}
+	
+	$scope.codeName = 'Yoda';
+	
+	$scope.count = 0;
+	
+	$scope.employees = [
+		{
+			fname : 'Roger',
+			lname : 'Smith',
+			salary : '50000'
+		},
+		{
+			fname : 'Sam',
+			lname : 'Smith',
+			salary : '30000'
+		},
+		{
+			fname : 'Will',
+			lname : 'Smith',
+			salary : '40000'
+		},
+	]
 }])
