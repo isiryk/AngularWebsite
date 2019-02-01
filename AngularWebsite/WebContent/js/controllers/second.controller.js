ccd3442bb871a8857255443032f287488fd718ec
Ivan Siryk <siryk.ivan@gmail.com>
2019-01-31 21:07:08 -0500

    Created a form for option 2

--- AngularWebsite/WebContent/js/controllers/second.controller.js
+++ AngularWebsite/WebContent/js/controllers/second.controller.js
@@ -1,5 +1,25 @@
 var mainApp = angular.module('mainApp');
 
 mainApp.controller("SecondController", ['$scope', function($scope){
-	$scope.test = "test";
+	$scope.form = {};
+	
+	$scope.addReview = function(){
+		$scope.reviews.push($scope.form);
+		$scope.form = {};
+	}
+	
+	$scope.reviews = [
+		{
+			"comment" : "Great food",
+			"by" : "Ivan@email.com"
+		},
+		{
+			"comment" : "Amazing food",
+			"by" : "Alex@email.com"
+		},
+		{
+			"comment" : "Cool food",
+			"by" : "John@email.com"
+		},
+	]
 }]);
